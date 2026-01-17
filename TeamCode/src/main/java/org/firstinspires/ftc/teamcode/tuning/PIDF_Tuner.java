e8e4026307b97e86d516939becaf2d150cd067c9
JacksonBuissness <jwlor@outlook.com>
2026-01-17 14:09:55 -0800

    Whole Pidf stupid thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/PIDF_Tuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/PIDF_Tuner.java
@@ -37,6 +37,47 @@ public class PIDF_Tuner extends OpMode {
 
     @Override
     public void loop() {
+        //All gamepad command to add
+        //Set target Velo
+        //Update Telmet
+        if(gamepad1.triangleWasPressed()){
+            if (curTarVel==highVelocity){
+                curTarVel= lowVelocity;
+            } else {curTarVel=highVelocity;}
+        }
+        if(gamepad1.squareWasPressed()){
+            stepIndex=(stepIndex+1) % stepSizes.length;
+
+        }
+        if(gamepad1.dpadRightWasPressed()){
+            F += stepSizes[stepIndex];
+        }
+        if(gamepad1.dpadLeftWasPressed()){
+            F -=stepSizes[stepIndex];
+        }
+        if(gamepad1.dpadUpWasPressed()){
+            P += stepSizes[stepIndex];
+        }
+        if(gamepad1.dpadDownWasPressed()){
+            P -=stepSizes[stepIndex];
+        }
+        //set new PIDF coefficients
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P,0,0,F);
+        leftShootMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+
+        //velocity
+        leftShootMotor.setVelocity(curTarVel);
+        rightShootMotor.setVelocity(curTarVel);
+        double curVel= leftShootMotor.getVelocity();
+        double error= curTarVel-curVel;
+        telemetry.addData("Target Velocity", curTarVel);
+        telemetry.addData("Current Velocity", curVel);
+        telemetry.addData("Error", "%.2f", error);
+        telemetry.addData("Target Velocity", curTarVel);
+        telemetry.addLine();
+        telemetry.addData("P","%.4f(Dpad Up/Down)", P);
+        telemetry.addData("F","%.4f(Dpad Right/Left)", F);
+        telemetry.addData("Step Size","%.4f(Square)", stepSizes[stepIndex]);
 
     }
 }
