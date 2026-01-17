d4bfd601e1c3bf2296601ea3b2a394b3e2bdb1d4
emiraltug1 <emiraltug1@gmail.com>
2026-01-17 13:24:43 -0800

    added tuner classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/PIDF_Tuner.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.tuning;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// no usages
+@TeleOp
+public class PIDF_Tuner extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
